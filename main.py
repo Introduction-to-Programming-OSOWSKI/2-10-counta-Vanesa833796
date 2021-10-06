e84b249c32c9674b95840d437ba57de5e4a590fb
Vanesa833796 <833796@mystma.org>
2021-10-06 09:22:52 -0500

    countA assignment

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def countA(w):
+    count = 0
+    for i in range(0, len(w)):
+        if w[i] == "a":
+            count = count + 1
+        else:
+            count = count
+    return(count)
+print(countA("vanesa"))        
